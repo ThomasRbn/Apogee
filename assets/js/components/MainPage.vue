c63a9c8700f4f0c2b9ef2b6d6113d2f461b85ecc
Thomas <robineau1u@etu.univ-lorraine.fr>
2023-12-15 19:33:16 +0100

    Main page finished

--- assets/js/components/MainPage.vue
+++ assets/js/components/MainPage.vue
@@ -1,9 +1,32 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <h1>Index</h1>
+  <div class="container">
+    <div class="d-flex vh-90 justify-content-center align-items-center">
+      <div class="shadow p-0 bg-white rounded-5">
+        <div class="d-flex flex-row vh-50">
+          <div class="d-flex justify-content-center align-items-center">
+            <img class="m-1" src="@img/apogee.png" width="500">
+          </div>
+          <div class="d-flex flex-column justify-content-center align-items-center text-white p-4 rounded-end-5"
+               style="background: linear-gradient(to right, #2739ff, #007aff);">
+            <h1>Welcome to Apogée</h1><br>
+            <h2 class="text-center">A new way to shop</h2>
+            <h2 class="text-center">Straight from space</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.vh-90 {
+  height: 90vh;
+}
 
+.vh-50 {
+  height: 50vh;
+}
 </style>
