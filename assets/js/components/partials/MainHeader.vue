0ed1ae05c83f42e7a6ff03a57f9fc4ab8c57ecd5
Thomas Robineau <robineau1u@etu.univ-lorraine.fr>
2024-03-26 22:26:41 +0100

    Create home page

---
+++ assets/js/components/partials/MainHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-danger-subtle">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+<!--          <li class="nav-item">-->
+<!--            <a class="nav-link" href="/product">Products</a>-->
+<!--          </li>-->
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
